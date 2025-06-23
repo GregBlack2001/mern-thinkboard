09702e49367fec37d098113356e42afee5c7fd16
Greg Black <gjblack2001@gmail.com>
2025-06-23 10:48:11 +0100

    Add full CRUD controllers for notes (Thinkboard API): get all, get by id, create, update, and delete

---
+++ src/server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import notesRoutes from "./routes/notesRoutes.js";
+import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+const PORT = process.env.PORT || 5001;
+
+const app = express();
+
+connectDB();
+
+// midleware
+app.use(express.json());
+
+app.use("/api/notes", notesRoutes);
+
+
+
+app.listen(PORT, () => {
+    console.log("server stated on port: ", PORT);
+});
