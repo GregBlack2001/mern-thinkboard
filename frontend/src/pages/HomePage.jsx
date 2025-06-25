e4129aab2437ccc5ef376c162000f530987933d9
Greg Black <gjblack2001@gmail.com>
2025-06-25 17:48:41 +0100

    Creating home page

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Navbar from '../components/navbar'
+import RateLimitedUI from '../components/RateLimitedUI'
+import axios from 'axios'
+import toast from 'react-hot-toast';
 
 const HomePage = () => {
+  const [isRateLimited, setIsRateLimited] = useState(false);
+  const [notes,setNotes] = useState([]);
+  const [loading,setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchNotes() {
+      try {
+        const res = await axios.get("http://localhost:5001/api/notes");
+        console.log(res.data);
+        setNotes(res.data);
+        setIsRateLimited(false);
+      } catch (error) {
+        console.log("Error fetching notes");
+        if(error.resposonse?.status ===  429) {
+          setIsRateLimited(true)
+        } else {
+          toast.error("Failed to load notes");
+        }
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchNotes();
+  }, []);
+
+
   return (
-    <div>HomePage</div>
+    <div className='min-h-screen'>
+      <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
+    </div>
   )
 }
 
-export default HomePage
+export default HomePage;
