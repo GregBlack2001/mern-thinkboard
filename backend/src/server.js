a6ef1e6d3599559fa61dff02a9336e20108bf217
Greg Black <gjblack2001@gmail.com>
2025-06-25 17:50:13 +0100

    Enable CORS for frontend at http://localhost:5173 in backend server

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,26 @@
 import express from 'express';
+import dotenv from "dotenv";
+import cors from 'cors';
+
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
-import dotenv from "dotenv";
 import ratelimiter from "./middleware/rateLimiter.js";
 
 dotenv.config();
-const PORT = process.env.PORT || 5001;
 
+const PORT = process.env.PORT || 5001;
 const app = express();
 
-
+app.use(cors({
+    origin:"http://localhost:5173",
+}));
 // midleware
 app.use(express.json()); // This middleware will parse JSon boddies: req.body
+
 app.use(ratelimiter);
+
+
+
 //Simple custon middleware
 // app.use((req, res,next) => {
 //     console.log(`Req method is ${req.method} & Req URL is ${req.url}`);
